b47ab1d4d4e13269fb2d90c9e464bc2e8d059b57
dotun-code-god <dotun494@gmail.com>
2025-10-17 04:22:46 +0100

    feat: add NotFoundPage component and update routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import HomePage from "./pages/home";
 import GiftPage from "./pages/gift";
+import NotFoundPage from "./pages/not-found";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="*" element={<NotFoundPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/gift" element={<GiftPage />} />
       </Routes>
---
+++ src/pages/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+      <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
+      <p className="text-xl text-gray-600">Page Not Found</p>
+    </div>
+  );
+}
